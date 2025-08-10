cf69d3e5d5e78fda5ba47fa3755ad137a0d416dc
Pierre-Chanel Gauthier <pierrechanel.gauthier@gmail.com>
2025-08-10 23:59:11 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nom du cache
-const CACHE_NAME = 'briz-cache-v4';
+const CACHE_NAME = 'briz-cache-v5';
 
 // Fichiers à mettre en cache lors de l'installation
 // On met en cache les ressources de base pour que l'app se lance hors ligne.
